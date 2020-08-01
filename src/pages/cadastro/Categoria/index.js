ee6564447ba2bafaea3a5dd3ab8d4d74692fba46
leoomolina <leomolina0501@hotmail.com>
2020-08-01 00:45:50 -0300

    Configurando rotas do servidor de produção

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,7 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost') ? 'http://localhost:8080/categorias' : 'https://molinaflix.herokuapp.com/categorias';
     fetch(URL)
       .then(async (responseServer) => {
         const response = await responseServer.json();
