bc1b1c100f5e2cb0ac4706837f603fdd3e9cbbe5
leoomolina <leomolina0501@hotmail.com>
2020-08-01 00:18:38 -0300

    Implementação Back-end

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -28,6 +28,17 @@ function CadastroCategoria() {
       infosDoEvento.target.value,
     );
   }
+
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL)
+      .then(async (responseServer) => {
+        const response = await responseServer.json();
+        setCategorias([
+          ...response,
+        ]);
+      });
+  }, []);
 
   return (
     <PageDefault>
@@ -73,6 +84,12 @@ function CadastroCategoria() {
           Cadastrar
         </Button>
       </form>
+
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
 
       <ul>
         {categorias.map((categoria) => (
