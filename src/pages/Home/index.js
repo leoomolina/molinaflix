0ebbebfdfa950928c069038eb284ecfd3a73f399
leoomolina <leomolina0501@hotmail.com>
2020-08-02 07:29:49 -0300

    Ajustes na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,42 +1,51 @@
-import React from 'react';
-import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
+import React, { useEffect, useState } from 'react';
+import PageDefault from '../../components/PageDefault';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
+import categoriasRepository from '../../repositories/categorias';
 
 function Home() {
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+  useEffect(() => {
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasWithVideos) => {
+        setDadosIniciais(categoriasWithVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
   return (
-    <div style={{ background: '#141414' }}>
-      <Menu />
+    <PageDefault paddingAll={0}>
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
 
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription="Vamos dar uma voltinha? A Serra do Cipó, faz jus a um dos conjuntos naturais mais exuberantes do planeta.
-        A topografia acidentada e a grande quantidade de nascentes formam diversos rios, cachoeiras, cânions e cavernas de excepcional beleza natural.
-        A Serra do Cipó localiza-se na porção sul da Serra do Espinhaço"
-      />
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription="Vamos dar uma voltinha? A Serra do Cipó, faz jus a um dos conjuntos naturais mais exuberantes do planeta.
+            A topografia acidentada e a grande quantidade de nascentes formam diversos rios, cachoeiras, cânions e cavernas de excepcional beleza natural.
+            A Serra do Cipó localiza-se na porção sul da Serra do Espinhaço"
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+            </div>
+          );
+        }
 
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />
-
-      <Footer />
-    </div>
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
+    </PageDefault>
   );
 }
 
